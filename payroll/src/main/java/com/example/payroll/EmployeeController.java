36976a0b587e144db7680eb5bac0a44667ab39aa
Nicolas Jackel <nickjackel@gmail.com>
2026-08-07 21:24:26 -0300

    EmployeeAssembler implemented

--- payroll/src/main/java/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/com/example/payroll/EmployeeController.java
@@ -21,9 +21,11 @@ import jakarta.persistence.GeneratedValue;
 @RestController
 class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository){
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
@@ -31,15 +33,13 @@ class EmployeeController {
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
 
-        List<EntityModel<Employee>> employees = repository.findAll().stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
-        .collect(Collectors.toList());
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+            .map(assembler::toModel) //
+            .collect(Collectors.toList());
 
         return CollectionModel.of(employees, 
-        linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
-}
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
     // end::get-aggregate-root[]
 
     @PostMapping("/employees")
@@ -50,14 +50,13 @@ class EmployeeController {
     // Single item
 
     @GetMapping("/employees/{id}")
-    EntityModel<Employee> one(@PathVariable Long id){
-        Employee employee = repository.findById(id)
-        .orElseThrow(() -> new EmployeeNotFoundException(id));
+	EntityModel<Employee> one(@PathVariable Long id) {
 
-        return EntityModel.of(employee,
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
-    }
+		Employee employee = repository.findById(id) //
+				.orElseThrow(() -> new EmployeeNotFoundException(id));
+
+		return assembler.toModel(employee);
+	}
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
