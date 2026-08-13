91f9eead0f3bd59a28620d48440b9fbf23000605
Nicolas Jackel <nickjackel@gmail.com>
2026-08-13 20:46:09 -0300

    Adicionando repository e modelassembler

---
+++ livraria/src/main/java/com/nicolas/livraria/BookController.java
@@ -0,0 +1,25 @@
+package com.nicolas.livraria;
+
+package com.example.payroll;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import jakarta.persistence.GeneratedValue;
+
+class BookController {
+    
+}
